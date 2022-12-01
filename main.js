34f015d30bca3c6875f010bda0659824cbe3de3c
Dudebtm <83547201+Dudebtm@users.noreply.github.com>
2022-12-01 09:41:13 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+/*src="https://cdn.jsdelivr.net/npm/js-confetti@latest/dist/js-confetti.browser.js"
+const jsConfetti = new JSConfetti();
+
+      function confettiFY() {
+         jsConfetti.addConfetti({
+            emojis: ['👍', '😊', '😁'],
+            confettiRadius: 6,
+            confettiNumber: 500,
+         });
+      }
+      document.querySelector(".secret").addEventListener("click", confettiFY);
+*/
